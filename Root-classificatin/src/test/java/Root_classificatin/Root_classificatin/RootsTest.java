1e9f26ca15bcfa37c43709cf833129ab21b5f4ee
yasminta <87039796+yasminta@users.noreply.github.com>
2024-01-25 16:42:26 +0200

    version 3 - include tests

--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -10,7 +10,6 @@ public class RootsTest
 	@Test
 	public void testPartion1() 
 	{
-		//(0,1,2)
 		Roots.calculate_roots(-2, 1, 2);
 		
 		String  root_expected = Root_Types.ERROR.toString();
@@ -18,5 +17,40 @@ public class RootsTest
 		
 		assertEquals(root_expected, Roots.root_types().toString());
 	}
-	
+	@Test
+	public void Two_roots() 
+	{
+		Roots.calculate_roots(2, 50,10);
+		
+		String  root_expected = Root_Types.Two_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	@Test
+	public void Equal_roots() 
+	{
+		Roots.calculate_roots(1,20,100);
+		
+		String  root_expected = Root_Types.Equal_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	@Test
+	public void No_roots() 
+	{
+		Roots.calculate_roots(1,1,100);
+		
+		String  root_expected = Root_Types.No_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	@Test
+	public void Not_quadratic() 
+	{
+		Roots.calculate_roots(0,4,3);
+		
+		String  root_expected = Root_Types.Not_quadratic.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
 	}
